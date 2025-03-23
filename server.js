9e6efde34adebc030e79f699f077b7f541fcdd68
i-niranjan <niranjanchaudhari2004@gmail.com>
2025-03-23 09:54:15 +0530

    Debug Code

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./config/db");
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 dbConnect();
+
+app._router.stack.forEach((r) => {
+  if (r.route && r.route.path) {
+    console.log(r.route.path);
+  }
+});
 
 app.use("/api/users", require("./routes/userRoutes"));
 
